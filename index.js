72045a078ba8dfc5dbe250fb0e7a471716a040d7
nadiaamanda-22 <nadiafebriantuy@gmail.com>
2025-09-19 21:44:17 +0700

    update index and add 3 file public

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 import multer from "multer";
 import fs from "fs/promises";
 import cors from "cors";
+import { text } from "stream/consumers";
 
 const app = express();
 const upload = multer();
@@ -12,6 +13,7 @@ const ai = new GoogleGenAI({});
 //inisialisasi model AI
 const geminiModels = {
   text: "gemini-2.5-flash-lite",
+  chat: "gemini-2.5-pro",
   image: "gemini-2.5-flash",
   audio: "gemini-2.5-flash",
   document: "gemini-2.5-flash-lite",
@@ -21,7 +23,9 @@ const geminiModels = {
 app.use(cors()); // .use() digunakan untuk panggil/bikin middleware
 app.use(express.json()); //untuk membolehkan kita menggunakan 'Content-Type: application/json' di header
 
-//inisialisasi route
+app.use(express.static("public")); //digunakan untuk ketika diakses di localhost akan otomatis mengarah ke folder yg dicantumkan
+
+//ENDPOINT 1 : generate text
 app.post("/generate-text", async (req, res) => {
   //handle bagaimana request diterima oleh user
   const { body } = req; // object destructuring
@@ -52,6 +56,69 @@ app.post("/generate-text", async (req, res) => {
     contents: message,
   });
   res.status(200).json({ reply: response.text });
+});
+
+//ENDPOINT 2 : chat
+app.post("/chat", async (req, res) => {
+  const { conversation } = req.body;
+
+  //pengeecekan 1 : conversation array atau bukan
+  if (!conversation || !Array.isArray(conversation)) {
+    res.status(400).json({
+      success: false,
+      data: null,
+      message: "Percakapan tidak valid!",
+    });
+  }
+
+  //pengecekan 2 : integritas datanya
+  let dataIsInvalid = false;
+  conversation.forEach((item) => {
+    if (!item) {
+      dataIsInvalid = true;
+    } else if (typeof item !== "object") {
+      dataIsInvalid = true;
+    } else if (!item.role || !item.message) {
+      dataIsInvalid = true;
+    }
+  });
+
+  if (dataIsInvalid) {
+    res.status(400).json({
+      success: false,
+      data: null,
+      message: "Ada data yang invalid pada percakapan yang dikirim!",
+    });
+    return;
+  }
+
+  //proses mapping
+  const contents = conversation.map((item) => {
+    return {
+      role: item.role,
+      parts: [{ text: item.message }],
+    };
+  });
+
+  try {
+    const aiResponse = await ai.models.generateContent({
+      model: geminiModels.chat,
+      contents,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: aiResponse.text,
+      message: null,
+    });
+  } catch (e) {
+    console.log({ e });
+    return res.status(500).json({
+      success: false,
+      data: null,
+      message: e.message,
+    });
+  }
 });
 
 // async function main() {
